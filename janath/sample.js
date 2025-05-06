31818bc61679da465c49b261c825cd4d0d2a9c45
janath-kumar <benzenehunt@gmail.com>
2025-05-06 07:53:26 +0530

    port changed to 9000

--- janath/sample.js
+++ janath/sample.js
@@ -3,7 +3,7 @@ const http = require('https');
 
 // Define the hostname and port
 const hostname = '127.0.0.1';
-const port = 3000;
+const port = 9000;
 
 // Create the server
 const server = http.createServer((req, res) => {
