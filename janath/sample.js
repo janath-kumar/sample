6fda8ee5dce8a65ce6ca30ccd165c5011d2f8583
janath-kumar <benzenehunt@gmail.com>
2025-05-06 07:50:10 +0530

    Revert "port changed"
    This reverts commit 52fcd8304b16249d59533a2f962faf454df85d9a.

--- janath/sample.js
+++ janath/sample.js
@@ -3,7 +3,7 @@ const http = require('https');
 
 // Define the hostname and port
 const hostname = '127.0.0.1';
-const port = 8080;
+const port = 3000;
 
 // Create the server
 const server = http.createServer((req, res) => {
